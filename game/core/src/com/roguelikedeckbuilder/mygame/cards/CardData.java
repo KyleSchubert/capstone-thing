be06f6cf732e0b5ad4480b93f2f7586abc14a17c
KyleSchubert <jedikyle5@gmail.com>
2024-04-30 23:27:02 -0400

    Icons for all cards

--- game/core/src/com/roguelikedeckbuilder/mygame/cards/CardData.java
+++ game/core/src/com/roguelikedeckbuilder/mygame/cards/CardData.java
@@ -69,25 +69,25 @@ public class CardData {
                     abilityTypeName = AbilityTypeName.DEFAULT_ATTACK;
                     upgradedAbilityTypeName = AbilityTypeName.DEFAULT_ATTACK_UPGRADED;
                     value = 40;
-                    iconFileName = "not done.png";
+                    iconFileName = "slash.png";
                 }
                 case ENERGY_SLICES -> {
                     abilityTypeName = AbilityTypeName.ENERGY_SLICES;
                     upgradedAbilityTypeName = AbilityTypeName.ENERGY_SLICES_UPGRADED;
                     value = 110;
-                    iconFileName = "not done.png";
+                    iconFileName = "energy slices.png";
                 }
                 case STRIKE -> {
                     abilityTypeName = AbilityTypeName.STRIKE;
                     upgradedAbilityTypeName = AbilityTypeName.STRIKE_UPGRADED;
                     value = 130;
-                    iconFileName = "not done.png";
+                    iconFileName = "strike.png";
                 }
                 case DEFEND -> {
                     abilityTypeName = AbilityTypeName.DEFEND;
                     upgradedAbilityTypeName = AbilityTypeName.DEFEND_UPGRADED;
                     value = 80;
-                    iconFileName = "not done.png";
+                    iconFileName = "defend.png";
                 }
                 case OUT_OF_STOCK -> {
                     abilityTypeName = AbilityTypeName.NOTHING;
@@ -98,19 +98,19 @@ public class CardData {
                 case DISCARD_DRAW -> {
                     abilityTypeName = AbilityTypeName.DISCARD_DRAW;
                     upgradedAbilityTypeName = AbilityTypeName.DRAW;
-                    iconFileName = "not done.png";
+                    iconFileName = "discard draw.png";
                     value = 65;
                 }
                 case WEIRD_PUNCH -> {
                     abilityTypeName = AbilityTypeName.PERCENTAGE_PUNCH;
                     upgradedAbilityTypeName = AbilityTypeName.PERCENTAGE_PUNCH_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "weird punch.png";
                     value = 105;
                 }
                 case AMPLIFY -> {
                     abilityTypeName = AbilityTypeName.AMPLIFY;
                     upgradedAbilityTypeName = AbilityTypeName.AMPLIFY_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "amplify.png";
                     value = 250;
                 }
                 case ROUTINE_POKING -> {
@@ -146,61 +146,61 @@ public class CardData {
                 case PRE_CURE -> {
                     abilityTypeName = AbilityTypeName.PRE_CURE;
                     upgradedAbilityTypeName = AbilityTypeName.PRE_CURE_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "pre cure.png";
                     value = 110;
                 }
                 case MAGIC_BARRIER -> {
                     abilityTypeName = AbilityTypeName.MAGIC_BARRIER;
                     upgradedAbilityTypeName = AbilityTypeName.MAGIC_BARRIER_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "magic barrier.png";
                     value = 270;
                 }
                 case FRYING_PAN -> {
                     abilityTypeName = AbilityTypeName.FRYING_PAN;
                     upgradedAbilityTypeName = AbilityTypeName.FRYING_PAN_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "frying pan.png";
                     value = 150;
                 }
                 case TETANUS -> {
                     abilityTypeName = AbilityTypeName.TETANUS;
                     upgradedAbilityTypeName = AbilityTypeName.TETANUS_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "tetanus.png";
                     value = 190;
                 }
                 case NAILS_ON_CHALKBOARD -> {
                     abilityTypeName = AbilityTypeName.NAILS_ON_CHALKBOARD;
                     upgradedAbilityTypeName = AbilityTypeName.NAILS_ON_CHALKBOARD_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "nails on chalkboard.png";
                     value = 210;
                 }
                 case CURSE -> {
                     abilityTypeName = AbilityTypeName.CURSE;
                     upgradedAbilityTypeName = AbilityTypeName.CURSE_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "curse.png";
                     value = 250;
                 }
                 case HEX -> {
                     abilityTypeName = AbilityTypeName.HEX;
                     upgradedAbilityTypeName = AbilityTypeName.HEX_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "hex.png";
                     value = 250;
                 }
                 case DEBILITATE -> {
                     abilityTypeName = AbilityTypeName.DEBILITATE;
                     upgradedAbilityTypeName = AbilityTypeName.DEBILITATE_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "debilitate.png";
                     value = 300;
                 }
                 case SPREAD_GERMS -> {
                     abilityTypeName = AbilityTypeName.SPREAD_GERMS;
                     upgradedAbilityTypeName = AbilityTypeName.SPREAD_GERMS_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "spread germs.png";
                     value = 200;
                 }
                 case CRACK_KNUCKLES -> {
                     abilityTypeName = AbilityTypeName.CRACK_KNUCKLES;
                     upgradedAbilityTypeName = AbilityTypeName.CRACK_KNUCKLES_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "crack knuckles.png";
                     value = 290;
                 }
                 case THORNS -> {
@@ -224,103 +224,103 @@ public class CardData {
                 case SPIKED_SHIELD -> {
                     abilityTypeName = AbilityTypeName.SPIKED_SHIELD;
                     upgradedAbilityTypeName = AbilityTypeName.SPIKED_SHIELD_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "spiked shield.png";
                     value = 180;
                 }
                 case MULTI_STAB -> {
                     abilityTypeName = AbilityTypeName.MULTI_STAB;
                     upgradedAbilityTypeName = AbilityTypeName.MULTI_STAB_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "multi stab.png";
                     value = 170;
                 }
                 case RAMPING -> {
                     abilityTypeName = AbilityTypeName.RAMPING;
                     upgradedAbilityTypeName = AbilityTypeName.RAMPING_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "ramping.png";
                     value = 230;
                 }
                 case SWIFT_BLOCK -> {
                     abilityTypeName = AbilityTypeName.SWIFT_BLOCK;
                     upgradedAbilityTypeName = AbilityTypeName.SWIFT_BLOCK_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "swift block.png";
                     value = 220;
                 }
                 case BURN_STEEL_WOOL -> {
                     abilityTypeName = AbilityTypeName.BURN_STEEL_WOOL;
                     upgradedAbilityTypeName = AbilityTypeName.BURN_STEEL_WOOL_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "burn steel wool.png";
                     value = 260;
                 }
                 case VERY_ILL -> {
                     abilityTypeName = AbilityTypeName.VERY_ILL;
                     upgradedAbilityTypeName = AbilityTypeName.VERY_ILL_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "very ill.png";
                     value = 260;
                 }
                 case REGENERATE -> {
                     abilityTypeName = AbilityTypeName.REGENERATE;
                     upgradedAbilityTypeName = AbilityTypeName.REGENERATE_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "regenerate.png";
                     value = 180;
                 }
                 case SCATTERSHOT -> {
                     abilityTypeName = AbilityTypeName.SCATTERSHOT;
                     upgradedAbilityTypeName = AbilityTypeName.SCATTERSHOT_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "scattershot.png";
                     value = 215;
                 }
                 case CULL -> {
                     abilityTypeName = AbilityTypeName.CULL;
                     upgradedAbilityTypeName = AbilityTypeName.CULL_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "cull.png";
                     value = 330;
                 }
                 case LIFE_LEECHING -> {
                     abilityTypeName = AbilityTypeName.LIFE_LEECHING;
                     upgradedAbilityTypeName = AbilityTypeName.LIFE_LEECHING_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "life leeching.png";
                     value = 220;
                 }
                 case AFFLICTION -> {
                     abilityTypeName = AbilityTypeName.AFFLICTION;
                     upgradedAbilityTypeName = AbilityTypeName.AFFLICTION_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "affliction.png";
                     value = 160;
                 }
                 case HOT_HOLY_WATER -> {
                     abilityTypeName = AbilityTypeName.HOT_HOLY_WATER;
                     upgradedAbilityTypeName = AbilityTypeName.HOT_HOLY_WATER_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "hot holy water.png";
                     value = 190;
                 }
                 case TWO_SHIELDS -> {
                     abilityTypeName = AbilityTypeName.TWO_SHIELDS;
                     upgradedAbilityTypeName = AbilityTypeName.TWO_SHIELDS_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "two shields.png";
                     value = 180;
                 }
                 case BOULDER_THROW -> {
                     abilityTypeName = AbilityTypeName.BOULDER_THROW;
                     upgradedAbilityTypeName = AbilityTypeName.BOULDER_THROW_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "boulder throw.png";
                     value = 300;
                 }
                 case SYMPATHETIC_PAIN -> {
                     abilityTypeName = AbilityTypeName.SYMPATHETIC_PAIN;
                     upgradedAbilityTypeName = AbilityTypeName.SYMPATHETIC_PAIN_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "sympathetic pain.png";
                     value = 220;
                 }
                 case DUST -> {
                     abilityTypeName = AbilityTypeName.DUST;
                     upgradedAbilityTypeName = AbilityTypeName.DUST_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "dust.png";
                     value = 270;
                 }
                 case DISTRACTION -> {
                     abilityTypeName = AbilityTypeName.DISTRACTION;
                     upgradedAbilityTypeName = AbilityTypeName.DISTRACTION_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "distraction.png";
                     value = 210;
                 }
                 case FIRE_BREATH -> {
@@ -332,31 +332,31 @@ public class CardData {
                 case COIN_WALL -> {
                     abilityTypeName = AbilityTypeName.COIN_WALL;
                     upgradedAbilityTypeName = AbilityTypeName.COIN_WALL_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "coin wall.png";
                     value = 370;
                 }
                 case FOIL_ARMOR -> {
                     abilityTypeName = AbilityTypeName.FOIL_ARMOR;
                     upgradedAbilityTypeName = AbilityTypeName.FOIL_ARMOR_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "foil armor.png";
                     value = 130;
                 }
                 case BLOCK_AND_STUFF -> {
                     abilityTypeName = AbilityTypeName.BLOCK_AND_STUFF;
                     upgradedAbilityTypeName = AbilityTypeName.BLOCK_AND_STUFF_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "block and stuff.png";
                     value = 220;
                 }
                 case NOTHING_BUT_GOOD -> {
                     abilityTypeName = AbilityTypeName.NOTHING_BUT_GOOD;
                     upgradedAbilityTypeName = AbilityTypeName.NOTHING_BUT_GOOD_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "nothing but huh.png";
                     value = 600;
                 }
                 case NOTHING_BUT_GOLD -> {
                     abilityTypeName = AbilityTypeName.NOTHING_BUT_GOLD;
                     upgradedAbilityTypeName = AbilityTypeName.NOTHING_BUT_GOLD_UPGRADED;
-                    iconFileName = "not done.png";
+                    iconFileName = "nothing but huh.png";
                     value = 600;
                 }
                 default ->
